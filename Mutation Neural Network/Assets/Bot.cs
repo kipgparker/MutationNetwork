61f5229af17a3b4dceff24c5899913968e3d086c
Kip <kipsterbroz@gmail.com>
2019-03-31 14:24:44 +0100

    First commit
    NN project

---
+++ Mutation Neural Network/Assets/Bot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bot : MonoBehaviour
+{
+    public float speed;//Speed Multiplier
+    public float rotation;//Rotation multiplier
+    public LayerMask raycastMask;//Mask for the sensors
+
+    private float[] input = new float[5];//input to the neural network
+    public NeuralNetwork network;
+
+    public int position;//Checkpoint number on the course
+    public bool collided;//To tell if the car has crashed
+
+    void FixedUpdate()//FixedUpdate is called at a constant interval
+    {
+        if (!collided)//if the car has not collided with the wall, it uses the neural network to get an output
+        {
+            for (int i = 0; i < 5; i++)//draws five debug rays as inputs
+            {
+                Vector3 newVector = Quaternion.AngleAxis(i * 45 - 90, new Vector3(0, 1, 0)) * transform.right;//calculating angle of raycast
+                RaycastHit hit;
+                Ray Ray = new Ray(transform.position, newVector);
+
+                if (Physics.Raycast(Ray, out hit, 10, raycastMask))
+                {
+                    input[i] = (10 - hit.distance) / 10;//return distance, 1 being close
+                }
+                else
+                {
+                    input[i] = 0;//if nothing is detected, will return 0 to network
+                }
+            }
+
+            float[] output = network.FeedForward(input);//Call to network to feedforward
+        
+            transform.Rotate(0, output[0] * rotation, 0, Space.World);//controls the cars movement
+            transform.position += this.transform.right * output[1] * speed;//controls the cars turning
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("CheckPoint"))//check if the car passes a gate
+        {
+            GameObject[] checkPoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+            for (int i=0; i < checkPoints.Length; i++)
+            {
+                if(collision.collider.gameObject == checkPoints[i] && i == (position + 1 + checkPoints.Length) % checkPoints.Length)
+                {
+                    position++;//if the gate is one ahead of it, it increments the position, which is used for the fitness/performance of the network
+                    break;
+                }
+            }
+        }
+        else if(collision.collider.gameObject.layer != LayerMask.NameToLayer("Learner"))
+        {
+            collided = true;//stop operation if car has collided
+        }
+    }
+
+
+    public void UpdateFitness()
+    {
+        network.fitness = position;//updates fitness of network for sorting
+    }
+}
---
+++ Mutation Neural Network/Assets/Manager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public float timeframe;
+    public int populationSize;//creates population size
+    public GameObject prefab;//holds bot prefab
+
+    public int[] layers = new int[3] { 5, 3, 2 };//initializing network to the right size
+
+    [Range(0.0001f, 1f)] public float MutationChance = 0.01f;
+
+    [Range(0f, 1f)] public float MutationStrength = 0.5f;
+
+    [Range(0.1f, 10f)] public float Gamespeed = 1f;
+
+    //public List<Bot> Bots;
+    public List<NeuralNetwork> networks;
+    private List<Bot> cars;
+
+    void Start()// Start is called before the first frame update
+    {
+        if (populationSize % 2 != 0)
+            populationSize = 50;//if population size is not even, sets it to fifty
+
+        InitNetworks();
+        InvokeRepeating("CreateBots", 0.1f, timeframe);//repeating function
+    }
+
+    public void InitNetworks()
+    {
+        networks = new List<NeuralNetwork>();
+        for (int i = 0; i < populationSize; i++)
+        {
+            NeuralNetwork net = new NeuralNetwork(layers);
+            net.Load("Assets/Save.txt");//on start load the network save
+            networks.Add(net);
+        }
+    }
+
+    public void CreateBots()
+    {
+        Time.timeScale = Gamespeed;//sets gamespeed, which will increase to speed up training
+        if (cars != null)
+        {
+            for (int i = 0; i < cars.Count; i++)
+            {
+                GameObject.Destroy(cars[i].gameObject);//if there are Prefabs in the scene this will get rid of them
+            }
+
+            SortNetworks();//this sorts networks and mutates them
+        }
+
+        cars = new List<Bot>();
+        for (int i = 0; i < populationSize; i++)
+        {
+            Bot car = (Instantiate(prefab, new Vector3(0, 1.6f, -16), new Quaternion(0, 0, 1, 0))).GetComponent<Bot>();//create botes
+            car.network = networks[i];//deploys network to each learner
+            cars.Add(car);
+        }
+        
+    }
+
+    public void SortNetworks()
+    {
+        for (int i = 0; i < populationSize; i++)
+        {
+            cars[i].UpdateFitness();//gets bots to set their corrosponding networks fitness
+        }
+        networks.Sort();
+        networks[populationSize - 1].Save("Assets/Save.txt");//saves networks weights and biases to file, to preserve network performance
+        for (int i = 0; i < populationSize / 2; i++)
+        {
+            networks[i] = networks[i + populationSize / 2].copy(new NeuralNetwork(layers));
+            networks[i].Mutate((int)(1/MutationChance), MutationStrength);
+        }
+    }
+}
